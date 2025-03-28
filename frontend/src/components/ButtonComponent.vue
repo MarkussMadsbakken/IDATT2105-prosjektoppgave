1a6f9652d99a08c53e9ec88d87d3bbe387f5d77c
Jacob Lein <jacoble@stud.ntnu.no>
2025-03-28 11:51:53 +0100

    Create primary- and secondaryButton components

---
+++ frontend/src/components/ButtonComponent.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+
+const props = withDefaults(defineProps<{
+  variant : 'primary' | 'secondary'
+  width?: string
+  height?: string
+}>(), {
+  width: '200px',
+  height: '50px'
+})
+
+</script>
+<template>
+  <button
+  :class="['base-button',variant]"
+  :style="{width: props.width, height: props.height}"
+  >
+    <slot />
+  </button>
+</template>
+
+<style>
+
+.base-button {
+  all: unset;
+  font-weight: bold;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0,0,0,0.2);
+  padding: 0.5rem 1rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.base-button:hover{
+  cursor: pointer;
+}
+.primary{
+  background-color: var(--color-primaryButton);
+  color: white;
+}
+.secondary{
+  background-color: var(--color-secondaryButton);
+  color: black;
+}
+
+</style>
