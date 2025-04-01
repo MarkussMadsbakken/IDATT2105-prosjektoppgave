840ac1f97d6c04eaf71e74a0f985959d9bcf9d8e
aTrueYety <erikhoff00@gmail.com>
2025-04-01 16:27:49 +0200

    add endpoint to create new messages in MessageController

--- backend/src/main/java/edu/ntnu/stud/controller/MessageController.java
+++ backend/src/main/java/edu/ntnu/stud/controller/MessageController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +30,16 @@ public class MessageController {
    */
   public MessageController(MessageService messageService) {
     this.messageService = messageService;
+  }
+
+  /**
+   * Adds a new message to the system.
+   *
+   * @param message the message to be added
+   */
+  @PostMapping
+  public void addMessage(@RequestBody Message message) {
+    messageService.addMessage(message);
   }
 
   /**
