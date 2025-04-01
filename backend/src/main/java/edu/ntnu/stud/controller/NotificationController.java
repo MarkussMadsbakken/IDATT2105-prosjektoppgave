ffafb903688a02f4d9499814937bc4caa127f05b
aTrueYety <erikhoff00@gmail.com>
2025-04-01 11:54:03 +0200

    Add Notification Controller

---
+++ backend/src/main/java/edu/ntnu/stud/controller/NotificationController.java
@@ -0,0 +1,69 @@
+package edu.ntnu.stud.controller;
+
+import edu.ntnu.stud.model.Notification;
+import edu.ntnu.stud.service.NotificationService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This is a controller class for managing notifications in the system.
+ * It provides endpoints to retrieve, and list notifications, but does not include
+ * endpoints for creating notifications as these are handled internally.
+ */
+@RestController
+@RequestMapping("/api/auth")
+public class NotificationController {
+
+  @Autowired
+  private NotificationService notificationService;
+
+  /**
+   * Initializes the NotificationController with a NotificationService instance.
+   *
+   * @param notificationService the NotificationService instance to be used
+   */
+  public NotificationController(NotificationService notificationService) {
+    this.notificationService = notificationService;
+  }
+
+  /**
+   * Retrieves all notifications in the system.
+   *
+   * @return a list of all Notification objects
+   */
+  public List<Notification> getAllNotifications() {
+    return notificationService.getAllNotifications();
+  }
+
+  /**
+   * Retrieves all notifications for a specific user.
+   *
+   * @param userId the ID of the user whose notifications are to be retrieved
+   * @return a list of Notification objects for the specified user
+   */
+  public List<Notification> getNotificationsByUserId(long userId) {
+    return notificationService.getNotificationsByUserId(userId);
+  }
+
+  /**
+   * Retrieves a notification by its ID.
+   *
+   * @param id the ID of the notification to be retrieved
+   * @return the Notification object with the specified ID, or null if not found
+   */
+  public Notification getNotificationById(long id) {
+    return notificationService.getNotificationById(id);
+  }
+
+  /**
+   * Marks a notification as read.
+   *
+   * @param id the ID of the notification to be marked as read
+   */
+  public void markNotificationAsRead(long id) {
+    notificationService.markNotificationAsRead(id);
+  }
+  
+}
