836aeb3c5d8bab0dcbd0b55b200f2bc6da2d3d8e
aTrueYety <erikhoff00@gmail.com>
2025-04-02 12:39:31 +0200

    Add authorization check for message sender in addMessage method

--- backend/src/main/java/edu/ntnu/stud/controller/MessageController.java
+++ backend/src/main/java/edu/ntnu/stud/controller/MessageController.java
@@ -1,13 +1,18 @@
 package edu.ntnu.stud.controller;
 
 import edu.ntnu.stud.model.Message;
+import edu.ntnu.stud.service.JWTService;
 import edu.ntnu.stud.service.MessageService;
+
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +27,10 @@ public class MessageController {
 
   @Autowired
   private MessageService messageService;
+  @Autowired
+  private JWTService jwtService;
+
+  Logger logger = LoggerFactory.getLogger(MessageController.class);
 
   /**
    * Initializes the MessageController with a MessageService instance.
@@ -38,7 +47,18 @@ public class MessageController {
    * @param message the message to be added
    */
   @PostMapping
-  public void addMessage(@RequestBody Message message) {
+  public void addMessage(
+      @RequestBody Message message, @RequestHeader("Authorization") String token) {
+    long senderFromToken = jwtService.extractUserId(token);
+    if (
+        !((senderFromToken == message.getByerId() && message.isSentByBuyer()) 
+        || (senderFromToken == message.getSellerId() && !message.isSentByBuyer()))
+    ) {
+      logger.error("Sender ID does not match the message sender.");
+      throw new IllegalArgumentException("Sender ID does not match the message sender.");
+    }
+    // more validation can be added here
+    logger.info("Adding message: {}", message);
     messageService.addMessage(message);
   }
 
