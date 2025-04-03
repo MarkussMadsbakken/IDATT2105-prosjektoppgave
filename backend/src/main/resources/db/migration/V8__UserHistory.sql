c4efd7c9b92a39758c3777712ca5dd5c97a2dafe
aTrueYety <erikhoff00@gmail.com>
2025-04-03 16:06:42 +0200

    Rename foreign key constraints in user_history table migration for clarity

--- backend/src/main/resources/db/migration/V8__UserHistory.sql
+++ backend/src/main/resources/db/migration/V8__UserHistory.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS user_history (
   user_id BIGINT NOT NULL,
   listing_id CHAR(36) NOT NULL,
   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id),
-  CONSTRAINT fk_listing FOREIGN KEY (listing_id) REFERENCES listings(uuid)
+  CONSTRAINT fk_user_history FOREIGN KEY (user_id) REFERENCES users(id),
+  CONSTRAINT fk_listing_history FOREIGN KEY (listing_id) REFERENCES listings(uuid)
 );
