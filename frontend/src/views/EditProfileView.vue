1d44b1e10098254afd94c5ac003e3607a0a282a5
Jacob Lein <jacoble@stud.ntnu.no>
2025-04-03 19:20:53 +0200

    Create edit profile view

--- frontend/src/views/EditProfileView.vue
+++ frontend/src/views/EditProfileView.vue
@@ -1,50 +1,139 @@
 <script setup lang="ts">
-import UserImage from "@/components/UserImage.vue";
-import type { User } from '@/types'
-const user: User = {id: 1, username:"Jackyboy", firstName: "Jacob", lastName:"Lein", createdAt:new Date('2002-08-12'), isAdmin: true};
+import Fetch from "@/util/fetch";
+import useUpdateUser from '@/actions/user.ts';
+import Button from '@/components/Button.vue';
+import ImageSelector from '@/components/ImageSelector.vue';
+import TextInput from '@/components/TextInput.vue';
+import {onMounted, ref, watch} from 'vue';
+import {useAuth} from "@/stores/auth.ts";
+import { useGetUser } from '@/actions/user';
+import {computed} from "vue";
+
+
+const firstName = ref("");
+const lastName = ref("");
+const userName = ref("");
+const profileImage = ref<File | null>(null);
+const userId = useAuth().userId
+
+if (!userId) {
+  throw new Error("Bruker-ID mangler");
+}
+const {
+  data,
+  isError,
+  error,
+  isPending
+} = useGetUser(userId);
+
+const {
+  mutate: updateUser,
+  isPending: isUpdatingUser,
+  isError: isUpdateError,
+  error: updateError
+} = useUpdateUser({
+  onSuccess: () => {
+    console.log("Brukerprofil oppdatert!");
+  },
+});
+
+
+const profileImagePreview = computed(() => {
+  return profileImage.value ? URL.createObjectURL(profileImage.value) : null;
+});
+
+const onImageSelected = (files: File[]) => {
+  if (files.length>0){
+    profileImage.value=files[0];
+  }
+};
+watch(data, (user) =>{
+  if(user) {
+    firstName.value =user.firstName;
+    lastName.value = user.lastName;
+    userName.value = user.username;
+  }
+  })
+const onSubmit = () => {
+  updateUser({
+    username: userName.value,
+    firstName: firstName.value,
+    lastName: lastName.value,
+    profileImage: profileImage.value ?? undefined,
+  });
+}
 
 </script>
 
 <template>
-  <div class="title-wrapper">
-      <div class="title">Min profil</div>
-  </div>
-  <div class="user-box">
-    <div class="user-info-box">
-      <img v-if="user.imageUrl" :src="user.imageUrl" alt="Profilbilde"/>
-      <UserImage v-else :size="120" stroke-width="1.5" />
-      <h3 class="username">{{user.username}}</h3>
+  <div class="outer-wrapper">
+    <div class="page-title">Rediger profil</div>
+
+    <div class="listing-form">
+
+      <div class="form-group">
+        <label for="profileImage">Profilbilde</label>
+        <div class="image-upload-preview">
+          <ImageSelector @file-select="onImageSelected" />
+          <img v-if="profileImagePreview" :src="profileImagePreview" alt="Forhåndsvisning" width="120" />
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="firstName">Fornavn</label>
+        <TextInput v-model="firstName" type="text" id="firstName" />
+      </div>
+
+      <div class="form-group">
+        <label for="lastName">Etternavn</label>
+        <TextInput v-model="lastName" type="text" id="lastName" />
+      </div>
+      <div class="form-group">
+        <label for="userName">Brukernavn</label>
+        <TextInput v-model="userName" type="text" id="userName" />
+      </div>
+
+
+      <Button :label="'Lagre endringer'" variant="primary" @click="onSubmit" :disabled="isUpdatingUser">
+        Lagre endringer
+      </Button>
     </div>
   </div>
 </template>
 
 <style scoped>
-.user-box{
-  margin-top: 4rem;
-  display: flex;
-  margin-left: 4rem;
-  align-items: center;
-  border-radius: 5px;
-  width: 100%;
-  height: 8rem;
-  gap: 3rem;
-}
-.user-info-box{
+.form-group {
   display: flex;
   flex-direction: column;
-  gap: 1rem;
-  align-items: center;
-  justify-content: center;
-}
-.title{
-  font-size: 60px;
-  font-weight: bold;
-  text-align: center;
-}
-.title-wrapper{
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  gap: 0.25rem;
+  width: 100%;
 }
 
+.listing-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  margin-top: 1rem;
+  width: 50rem;
+}
+
+.outer-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 1rem;
+}
+.image-upload-preview{
+  display:flex;
+  flex-direction: row;
+  gap: 2rem;
+}
+.image-upload-preview img{
+  max-width: 200px;
+  max-height: 200px;
+  object-fit: contain;
+  border-radius: 6px;
+}
 </style>
