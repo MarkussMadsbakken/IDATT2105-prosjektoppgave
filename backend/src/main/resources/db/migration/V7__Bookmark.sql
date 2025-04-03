e8a194ba9d3373c0e3a9a39684d2dd421e88d69b
aTrueYety <erikhoff00@gmail.com>
2025-04-03 16:06:37 +0200

    Update foreign key constraints in bookmarks table migration

--- backend/src/main/resources/db/migration/V7__Bookmark.sql
+++ backend/src/main/resources/db/migration/V7__Bookmark.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS bookmarks (
   user_id BIGINT NOT NULL,
   listing_id CHAR(36) NOT NULL,
-  PRIMARY KEY (user_id, listing_id)
-  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id),
-  CONSTRAINT fk_listing FOREIGN KEY (listing_id) REFERENCES listings(uuid),
+  PRIMARY KEY (user_id, listing_id),
+  CONSTRAINT fk_user_bookmarks FOREIGN KEY (user_id) REFERENCES users(id),
+  CONSTRAINT fk_listing_bookmarks FOREIGN KEY (listing_id) REFERENCES listings(uuid)
 );
