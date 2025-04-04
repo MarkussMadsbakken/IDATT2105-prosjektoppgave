5e0c2100b54edede9ff4a44f13dd6a11daa7aebb
Jacob Lein <jacoble@stud.ntnu.no>
2025-04-04 12:19:42 +0200

    Start creating UserListing

---
+++ frontend/src/components/UserListing.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import {useAuth} from "@/stores/auth.ts";
+
+  const props = defineProps<{
+    userId: number;
+  }>();
+
+  const userID =useAuth().userId;
+
+</script>
+
+<template>
+
+
+</template>
+
+<style scoped>
+
+
+</style>
--- frontend/src/views/UserListingsView.vue
+++ frontend/src/views/UserListingsView.vue
@@ -1,4 +1,14 @@
+<script setup lang="ts">
+import UserListing from '@/components/UserListing.vue'
+import {useAuth} from "@/stores/auth.ts";
+
+  const userId= useAuth().userId;
+</script>
+
 <template>
-    Listings by user
-    222
+  <UserListing :userId="userId"></UserListing>
 </template>
+
+<style scoped>
+
+</style>
