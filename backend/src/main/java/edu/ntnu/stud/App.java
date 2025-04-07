cbeda87236d40cf208e5232fab050649fc955df7
aTrueYety <erikhoff00@gmail.com>
2025-04-07 18:10:18 +0200

    add OpenAPI definition for backend API documentation

--- backend/src/main/java/edu/ntnu/stud/App.java
+++ backend/src/main/java/edu/ntnu/stud/App.java
@@ -1,5 +1,7 @@
 package edu.ntnu.stud;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +12,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * Main application class for the Spring Boot application.
  */
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Backend API",
+        version = "1.0.0",
+        description = "This is the backend API for the project."
+    )
+)
 public class App {
 
   public static void main(String[] args) {
