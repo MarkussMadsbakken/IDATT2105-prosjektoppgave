eb3096f8e3eb6ea5224e79e42f3c111e9b71bf76
Markus <markussmadsbakken@gmail.com>
2025-04-08 23:47:28 +0200

    Update mapview with custom popup

--- frontend/src/views/MapView.vue
+++ frontend/src/views/MapView.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import L from "leaflet";
-import { onMounted, ref, watch } from "vue";
+import { createVNode, getCurrentInstance, onMounted, ref, render, watch } from "vue";
 import "leaflet/dist/leaflet.css";
 import { useGetAllListings } from "@/actions/getListing";
+import ListingMapPopup from "@/components/ListingMapPopup.vue";
+import { useRouter } from "vue-router";
 
 const mapRef = ref<HTMLDivElement | null>(null);
+const router = useRouter();
 
 const { data: listings } = useGetAllListings();
 
@@ -19,13 +22,29 @@ onMounted(() => {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
+    const instance = getCurrentInstance();
+
     // Watch for changes to listings and add markers
     watch(listings, (newListings) => {
         if (newListings) {
             newListings.forEach(listing => {
                 if (listing.latitude === 0 || listing.longitude === 0) return; // Skip if no coordinates
-                L.marker([listing.latitude, listing.longitude]).addTo(map)
-                    .bindPopup(listing.name || "Listing");
+                L.marker([listing.latitude, listing.longitude]).addTo(map).bindPopup(() => {
+                    const container = document.createElement('div');
+                    const node = createVNode(ListingMapPopup, {
+                        listing,
+                        position: {
+                            latitude: listing.latitude,
+                            longitude: listing.longitude
+                        },
+                        onClick: () => {
+                            router.push(`/listing/${listing.uuid}`);
+                        }
+                    });
+                    node.appContext = instance?.appContext!;
+                    render(node, container);
+                    return container;
+                });
             });
         }
     }, { immediate: true });
