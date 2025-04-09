72ee21e284d631a2f2256b56a9030e82c7eee1fb
Markus <markussmadsbakken@gmail.com>
2025-04-09 11:43:29 +0200

    Create userHistory api actions

---
+++ frontend/src/actions/userHistory.ts
@@ -0,0 +1,13 @@
+import { API_BASE_URL, type userHistoryRequest } from "@/types";
+import { FetchWithoutParse } from "@/util/fetch";
+
+export const pushUserHistory = (req: userHistoryRequest) => {
+    return FetchWithoutParse(`${API_BASE_URL}/user/history`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    }
+    );
+}
