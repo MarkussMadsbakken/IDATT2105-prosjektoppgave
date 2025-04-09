12a5edd49bf3042eb25a59cf714c52faae99b9ba
aTrueYety <erikhoff00@gmail.com>
2025-04-09 18:00:34 +0200

    Remove unused import statement from UserHistoryRepo

--- backend/src/main/java/edu/ntnu/stud/repo/UserHistoryRepo.java
+++ backend/src/main/java/edu/ntnu/stud/repo/UserHistoryRepo.java
@@ -3,7 +3,6 @@ package edu.ntnu.stud.repo;
 import edu.ntnu.stud.model.UserHistory;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
