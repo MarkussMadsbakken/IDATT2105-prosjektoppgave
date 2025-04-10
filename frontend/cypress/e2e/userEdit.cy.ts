3d25c4b243d30d97586b82070d8d5bd6348ff3e6
Markus <markussmadsbakken@gmail.com>
2025-04-10 19:47:47 +0200

    Fix useredit spec

--- frontend/cypress/e2e/userEdit.cy.ts
+++ frontend/cypress/e2e/userEdit.cy.ts
@@ -1,19 +1,24 @@
 describe("User Edit", () => {
   beforeEach(() => {
     cy.visit("/login");
+    cy.intercept("POST", "/api/auth/login").as("loginRequest");
+    cy.intercept('GET', '/api/user/2').as('getUser');
+    cy.intercept('POST', '/api/auth/change-credentials').as('updateUser');
 
     cy.get("form").within(() => {
       cy.get("input[name='username']").type("testuser1");
       cy.get("#password").type("password1");
       cy.get("button[type='submit']").click();
     });
-    cy.wait(500);
-    cy.get('.link.profile').click();
-    cy.get(".username").should("contain.text", "@testuser1");
-  });
-  it('should be able to edit picture and first- and last name', () => {
+
+    cy.wait("@loginRequest");
+    cy.url().should("include", "/");
     cy.visit('/profile');
-    cy.get('.settings-button').click();
+    cy.wait('@getUser');
+    cy.visit('/profile/edit')
+  });
+
+  it('should be able to edit picture and first- and last name', () => {
     cy.get("input[type='file']").should("exist");
     cy.get('[data-cy="drop-zone"]')
       .selectFile('cypress/fixtures/testImage2.png', {
@@ -27,29 +32,28 @@ describe("User Edit", () => {
     cy.get("#lastName").should('have.value', 'Klein');
     cy.get('[data-cy="save-button"]').click();
     cy.intercept('PUT', '/api/user/update').as('updateUser');
-    cy.wait(500);
     cy.get('.name').should('contain.text', 'Jakop Klein');
   });
+
   it('should be able to change username and password', () => {
-    cy.visit('/profile');
-    cy.get('.settings-button').click();
     cy.get('.edit-password-button').click();
     cy.get('input#userName').clear().type('jacoblein');
     cy.get('input[type="password"]#currentPassword').type('password1');
     cy.get('input[type="password"]#newPassword').type('mittpassord3');
     cy.get('input[type="password"]#repeatPassword').type('mittpassord3');
     cy.get('.save-password-button').click();
-    cy.intercept('PUT', '/api/user/update').as('updateUser');
+    cy.wait('@updateUser');
 
-    cy.wait(5000);
     cy.get('.logout-button').click();
     cy.get("form").within(() => {
       cy.get("input[name='username']").type("jacoblein");
       cy.get("#password").type("mittpassord3");
       cy.get("button[type='submit']").click();
     });
-    cy.wait(500);
-    cy.get('.link.profile').click();
+    cy.wait('@loginRequest');
+    cy.url().should("include", "/");
+    cy.visit('/profile');
+    cy.wait('@getUser');
     cy.get(".username").should("contain.text", "@jacoblein");
   })
 
